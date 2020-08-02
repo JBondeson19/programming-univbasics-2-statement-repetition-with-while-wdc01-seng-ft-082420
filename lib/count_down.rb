ec31c9bf2e7fd8aa8d31c2621853af38ac43d590
James P Bondeson <jpbond58@gmail.com>
2020-08-02 14:28:26 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,4 +1,8 @@
-# Write your code here
-while true do
-  puts "say this forever"
+magic_exit_number = 7
+count = 0
+while count < 10 do
+  break if count == magic_exit_number
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1
 end
+puts "Game Over pals"
